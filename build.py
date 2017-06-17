eef911d99d4f25a1feaa5471cb9aecc6cb934171
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 01:37:15 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
 def solution(dic):
-    '''Enter Code Here'''
+    '''Shortest way to get the sum of all the values from the dictionary'''
+    print sum(dic.values())
+    return sum(dic.values())
+
+solution({1:10, 2:20, 3:30})
