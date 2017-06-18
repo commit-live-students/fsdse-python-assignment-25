9051358153651328bc94dd37aeab2415b3de64f2
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 06:26:23 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
-def solution(dic):
-    '''Enter Code Here'''
+dic1={1:10, 2:20, 3:30}
+def solution(dic1):
+    sum=0
+    value=dic1.values()
+    for v in value:
+        sum=sum+v
+    return sum
+solution(dic1)
